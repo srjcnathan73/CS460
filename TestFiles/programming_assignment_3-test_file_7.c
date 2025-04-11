be17c703d05f29f2dc587cd1f0b1c2213640eec0
srjcnathan73 <nwsebastopol@hotmail.com>
2025-04-10 20:32:28 -0700

    Delete TestFiles directory

--- TestFiles/programming_assignment_3-test_file_7.c
+++
@@ -1,7 +0,0 @@
-// ***************************************************
-// * CS460: Programming Assignment 3: Test Program 7 *
-// ***************************************************
-procedure main (void)
-{
-  char char; // syntax error: reserved word "char" cannot be for the name of a variable
-}
